1bfbc6073b1f903575520ae08689c27819f0b3f3
Jeremy Vandehey <jgvandehey@gmail.com>
2015-01-16 23:11:48 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shitihear::Application.config.secret_token = 'b2f389c3537def74ff5f4f353e793267c1ff371e6bbacbd03743557a91689b9b9d73d0cfbb1c87d135b8318321b5b1c3568fe3d744bf439e441e4cc3092f4725'
